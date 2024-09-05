d45aba1d9f7d2ee2deb587c5a63086c048e33e3a
whgkswo <gjuk7238@gmail.com>
2024-09-05 17:14:39 +0900

    add dev branch

--- src/main/java/com/springboot/SpringStartApplication.java
+++ src/main/java/com/springboot/SpringStartApplication.java
@@ -3,6 +3,7 @@ package com.springboot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// dev브랜치 개시합니다~
 @SpringBootApplication
 public class SpringStartApplication {
   public static void main(String[] args) {
