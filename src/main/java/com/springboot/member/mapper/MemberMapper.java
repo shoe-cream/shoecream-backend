273cb8fcf215d0d48b0b3c47cc5750bddb599fef
pyyeon <pyyeon98@gmail.com>
2024-09-06 15:59:52 +0900

    member

---
+++ src/main/java/com/springboot/member/mapper/MemberMapper.java
@@ -0,0 +1,32 @@
+package com.springboot.member.mapper;
+
+import com.springboot.member.dto.MemberDto;
+import com.springboot.member.entity.Member;
+import org.mapstruct.Mapper;
+import org.mapstruct.ReportingPolicy;
+
+@Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
+public interface MemberMapper {
+
+    default Member memberPostToMember(MemberDto.Post requestBody){
+        Member member = new Member();
+        member.setEmail(requestBody.getEmail());
+        member.setPassword(requestBody.getPassword());
+        member.setName(requestBody.getName());
+        member.setEmployeeId(requestBody.getEmployeeId());
+        return member;
+    }
+
+    Member memberPatchToMember(MemberDto.Patch requestBody);
+
+
+    default Member memberPatchPasswordToMember(MemberDto.PatchPassword requestBody){
+        Member member = new Member();
+        member.setMemberId(requestBody.getMemberId());
+        member.setPassword(requestBody.getNewPassword());
+        return member;
+    }
+
+    ESZ
+
+}
