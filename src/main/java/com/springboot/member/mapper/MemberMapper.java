70a76061b02e91d949608f9d3227ff50e47ae11c
pyyeon <pyyeon98@gmail.com>
2024-09-10 15:17:06 +0900

    fix erroe

--- src/main/java/com/springboot/member/mapper/MemberMapper.java
+++ src/main/java/com/springboot/member/mapper/MemberMapper.java
@@ -27,8 +27,8 @@ public interface MemberMapper {
     @Mapping(source = "profileUrl", target = "profileUrl")
     Member profileUpdateToMember(MemberDto.Update profileUpdateDto);
 
-    @Mapping(source = "role", target = "member_roles.roles")
-    Member roleUpdateToMember(MemberDto.UpdateRole updateRoleDto);
+//    @Mapping(source = "role", target = "member_roles.roles")
+//    Member roleUpdateToMember(MemberDto.UpdateRole updateRoleDto);
 
     // Role 응답 DTO 매핑
     MemberDto.RoleResponse memberToRoleResponse(Member member);
