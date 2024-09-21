00f2d204be81d6c231ed9e7fd82a7e24c6264328
quokkavely <yellowjerri@gmail.com>
2024-09-21 16:08:07 +0900

    fix/sort(#210)

--- src/main/java/com/springboot/buyer_item/repository/BuyerItemQueryRepositoryImpl.java
+++ src/main/java/com/springboot/buyer_item/repository/BuyerItemQueryRepositoryImpl.java
@@ -1,18 +1,24 @@
 package com.springboot.buyer_item.repository;
 
 import com.querydsl.core.BooleanBuilder;
+import com.querydsl.core.types.Order;
+import com.querydsl.core.types.OrderSpecifier;
+import com.querydsl.core.types.dsl.PathBuilder;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import com.springboot.buyer.entity.QBuyer;
 import com.springboot.buyer_item.entity.BuyerItem;
 import com.springboot.buyer_item.entity.QBuyerItem;
 import com.springboot.item.entity.QItem;
+import com.springboot.manufacture.entity.QManufacture;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Repository;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 
 @Repository
@@ -49,8 +55,12 @@ public class BuyerItemQueryRepositoryImpl implements BuyerItemQueryRepositoryCus
             builder.and(buyerItem.item.itemNm.containsIgnoreCase(itemNm));
         }
 
+        //sort 처리
+        List<OrderSpecifier<?>> orderSpecifiers = getSortOrder(pageable, buyerItem);
+
         List<BuyerItem> results = queryFactory.selectFrom(buyerItem)
                 .where(builder)
+                .orderBy(orderSpecifiers.toArray(new OrderSpecifier[0]))
                 .offset(pageable.getOffset())
                 .limit(pageable.getPageSize())
                 .fetch();
@@ -79,8 +89,11 @@ public class BuyerItemQueryRepositoryImpl implements BuyerItemQueryRepositoryCus
                     .and(buyerItem.endDate.goe(startOfDay));
         }
 
+        List<OrderSpecifier<?>> orderSpecifiers = getSortOrder(pageable, buyerItem);
+
         List<BuyerItem> results = queryFactory.selectFrom(buyerItem)
                 .where(builder)
+                .orderBy(orderSpecifiers.toArray(new OrderSpecifier[0]))
                 .offset(pageable.getOffset())
                 .limit(pageable.getPageSize())
                 .fetch();
@@ -90,6 +103,15 @@ public class BuyerItemQueryRepositoryImpl implements BuyerItemQueryRepositoryCus
                 .fetchCount();
 
         return new PageImpl<>(results, pageable, total);
+    }
+
+    private List<OrderSpecifier<?>> getSortOrder(Pageable pageable, QBuyerItem buyerItem) {
+        List<OrderSpecifier<?>> orders = new ArrayList<>();
+        for (Sort.Order order : pageable.getSort()) {
+            PathBuilder pathBuilder = new PathBuilder(buyerItem.getType(), buyerItem.getMetadata());
+            orders.add(new OrderSpecifier(order.isAscending() ? Order.ASC : Order.DESC, pathBuilder.get(order.getProperty())));
+        }
+        return orders;
     }
 }
 
